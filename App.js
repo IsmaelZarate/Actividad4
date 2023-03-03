db094f7c6934563582d878af1f680d63781f62cf
IsmaelZárate <ismaelzarate386@gmail.com>
2023-03-03 00:14:16 -0500

    primer repositorio

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button,SafeAreaView, StyleSheet, TextInput, Text, View } from 'react-native';
 
 export default function App() {
+  const[materia_uno, setMateriaUno]= useState()
+  const[materia_dos, setMateriaDos]= useState()
+  const[materia_tres, setMateriaTres]= useState()
+  const getMaterias=(materia_uno,materia_dos,materia_tres) =>{
+    return materia_uno + "" + materia_dos + "" + materia_tres;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <View style={styles.container}>
+    <StatusBar style="auto"/>
+      <TextInput
+        style={styles.inputText}
+       placeholder="Introduce la primera Materia"
+       onChangeText={setMateriaUno}
+       value={materia_uno}
+      />
+     <TextInput
+        style={styles.inputText}
+       placeholder="Introduce la segunda Materia"
+       onChangeText={setMateriaDos}
+       value={materia_dos}
+      />
+        <TextInput
+        style={styles.inputText}
+       placeholder="Introduce la tercera Materia"
+       onChangeText={setMateriaTres}
+       value={materia_tres}
+      />
+      
+      <Button title="Carga de Materias" onePress={getMaterias} />
+      <Text style={styles.getMaterias}></Text>
+     
+   </View>
   );
-}
+ 
+};
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +48,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputText:{
+    width:"50%",
+    height:40,
+    borderWidth:1,
+    borderColor: '#FF0000',
+    marginVertical:15
+  }
 });
